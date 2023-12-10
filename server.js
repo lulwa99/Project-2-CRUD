0eb5ae37df1ac977b0fd02f5d9467738f394e5fe
lulwa <lulwafelaifel9@gmail.com>
2023-12-10 10:27:28 +0300

    srever.js create

---
+++ server.js
@@ -0,0 +1,62 @@
+// Dependncies
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+
+//initialize express
+const app = express()
+
+app.use(express.urlencoded({extended:ture}));
+app.use(express.json());
+
+// dotenv configration
+require('dotenv').config();
+//port configration 
+const port = process.env.PORT;
+
+//Nodejs to look for all statice  file in public folder 
+app.use(express.static("public"));
+
+//passport model config
+require('./config/passport');
+
+//set up middleware
+app.use(session ({
+    secret: process.env.SECRET,
+    resave:false,
+    saveUninitialized: tue
+}));
+
+//initialize passport 
+app.use(passport.initialize());
+// request session information
+app.use(passport.session());
+
+app.use(function(req,res,next){
+    res.locals.user = req.user;
+    next();
+})
+
+//Nodejs to look into the folder called views for all the ejs files
+app.set("view engine","ejs");
+
+//Database configration
+const db = require("./config/db");
+
+//import Routes
+const indexRouter = require("./routes/index");
+const productRouter = require("./routes/product");
+
+//mount routes
+app.use('/',indexRouter)
+app.use('/product',productRouter);
+
+
+//connection  to port
+app.listen(port,() =>{
+    console.log(`Construction app is running on ${port}`);
+})
+
+
+
+
