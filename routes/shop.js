e17bc699f98f79f07bdacb0d3e2beea265fdad94
kmajeed85 <cokhalid@outlook.com>
2023-12-10 16:58:24 +0300

    check

--- routes/shop.js
+++ routes/shop.js
@@ -14,11 +14,11 @@ const shopCntrl = require('../controllers/shop');
 // create get 
 
 
-router.get("/add"/*, isLoggedIn*/, shopCntrl.shop_create_get);
+router.get("/addS"/*, isLoggedIn*/, shopCntrl.shop_create_get);
 
 // create post 
 
-router.post("/add"/*, isLoggedIn*/, shopCntrl.shop_create_post)
+router.post("/addS"/*, isLoggedIn*/, shopCntrl.shop_create_post)
 
 
 
@@ -30,26 +30,26 @@ router.get("/", shopCntrl.shop_index_get)
 // show get 
 
 
-router.get("/detail", shopCntrl.shop_show_get)
+router.get("/detailS", shopCntrl.shop_show_get)
 
 
 // edit get 
 
 
-router.get("/edit", shopCntrl.shop_edit_get)
+router.get("/editS", shopCntrl.shop_edit_get)
 
 
 // delete get 
 
 
-router.get("/delete", shopCntrl.shop_delete_get)
+router.get("/deleteS", shopCntrl.shop_delete_get)
 
 
 
 // update put 
 
 
-router.post("/update", shopCntrl.shop_update_put)
+router.post("/updateS", shopCntrl.shop_update_put)
 
 
 
