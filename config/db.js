a0c42bbbca292a81a76de542fca5ce7fad21a213
YouKnow <hsn123.h7@gmail.com>
2023-12-10 13:45:03 +0300

    minor changes 2

---
+++ config/db.js
@@ -0,0 +1,13 @@
+// mongoose dependanciy
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// connection to the monogoDb cloud database
+mongoose.connect(process.env.DATABASEURL)
+.then(()=>{
+    const db = mongoose.connection
+    console.log(`MongoDB connected to DB ${db.name} at host ${db.host} at port ${db.port}!`);
+})
+.catch((err)=>{
+    console.log('MongoDB not connected '+err);
+})
